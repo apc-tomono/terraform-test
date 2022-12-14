2021904541a4a93a258897effa9522b51a87ae1a
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2022-12-14 16:37:19 +0900

    Change directory structure

---
+++ env/west/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.31.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tomono-terraform"
+    storage_account_name = "bcptestterraform"
+    container_name       = "tfstate-west"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
